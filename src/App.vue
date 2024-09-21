409d9fff96c68b1c6fa53e53c4a0df6ff222a21b
Avyukt27 <164896225+Avyukt27@users.noreply.github.com>
2024-09-21 21:56:01 +0530

    Created Basic Site

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="min-h-screen bg-white text-black dark:bg-black dark:text-white">
+    <h1 class="text-center text-2xl font-bold p-4">Welcome to My Page</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  mounted() {
+    // Check for user's preferred color scheme
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Add or remove the dark class based on the user's preference
+    if (darkModeMediaQuery.matches) {
+      document.documentElement.classList.add('dark');
+    }
+
+    // Listen for changes in the preference
+    darkModeMediaQuery.addEventListener('change', (event) => {
+      if (event.matches) {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
+    });
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+/* Additional styles if necessary */
 </style>
